d663f7262951ce27f935c8f510758570a4335490
Amin <98amiiin@gmail.com>
2025-07-24 20:06:48 +0330

    Add project files.

---
+++ HabibCool/Models/Agreement/Agreement.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HabibCool.Models.Agreement
+{
+    public class Agreement
+    {
+        [Key]
+        public int Id { get; set; }
+        public string AgreementCode { get; set; }
+        public string CarPlateNumber { get; set; }
+        public DateTime EnterDate { get; set; }
+        public DateTime ExitDate { get; set; }
+    }
+}
